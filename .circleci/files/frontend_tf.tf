39c3dc42a5d3cb032724f2d3d54036bc684cde68
ahmed-eldesoki <ahmedeldesoki22@gmail.com>
2023-11-13 20:59:34 +0200

    hh

--- .circleci/files/frontend_tf.tf
+++ .circleci/files/frontend_tf.tf
@@ -16,15 +16,6 @@ resource "aws_s3_bucket_website_configuration" "WebsiteConf" {
 
 }
 
-resource "aws_s3_bucket_public_access_block" "example" {
-  bucket = aws_s3_bucket.WebsiteBucket.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
-
 resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
   bucket = aws_s3_bucket.WebsiteBucket.id
   policy = data.aws_iam_policy_document.public_read_access.json
@@ -36,3 +27,15 @@ data "aws_iam_policy_document" "public_read_access" {
 	  type = "*"
 	  identifiers = ["*"]
 	}
+
+    actions = [
+      "s3:GetObject",
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      aws_s3_bucket.WebsiteBucket.arn,
+      "${aws_s3_bucket.WebsiteBucket.arn}/*",
+    ]
+  }
+}
