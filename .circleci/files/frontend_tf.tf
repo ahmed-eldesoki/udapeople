4e451c25b108d60929a664c3690363742e6f19e0
ahmed-eldesoki <ahmedeldesoki22@gmail.com>
2023-11-13 21:05:52 +0200

    teg

--- .circleci/files/frontend_tf.tf
+++ .circleci/files/frontend_tf.tf
@@ -3,6 +3,15 @@ resource "aws_s3_bucket" "WebsiteBucket" {
 
 }
 
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.WebsiteBucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
 resource "aws_s3_bucket_website_configuration" "WebsiteConf" {
   bucket = aws_s3_bucket.WebsiteBucket.id
 
